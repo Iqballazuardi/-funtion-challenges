4a34a19a50939a2d4c5d6512253ba135145a92d4
iqbal lazuardi <iqballazuardi12@gmail.com>
2021-06-26 15:52:17 +0700

    initial

---
+++ script.js
@@ -0,0 +1,32 @@
+function ShoutOut (a, b){
+    var A1 = a
+    var A2 = b
+    total = A1 + A2
+    return total
+}
+
+console.log(ShoutOut('halo', ' function'))
+
+function calculteMultiply(a, b){
+    var num1 = a;
+    var num2 = b;
+    var total;
+
+    total = num1 * num2;
+
+    return total;
+
+}
+console.log(calculteMultiply(5,6))
+
+function processSentence(){
+    var nama = 'iqbal'
+    var age = 22
+    var alamt = 'waypetai'
+    var hobi = 'basketball'
+    var fullSentence 
+    fullSentence = `nama saya ${nama} umur saya ${age} rumah saya di ${alamt}, dan hobi saya ${hobi}`
+    return fullSentence
+}
+
+console.log(processSentence())
